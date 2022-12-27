4f0584bc5777a4bf0bac53884152710b39fbc429
Golufem <oluwafemigbadero@gmail.com>
2022-12-27 23:05:33 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Header from "./Component/Header/Header"
-
+import Nav from './Component/Nav/Nav';
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Nav/>
     </div>
   );
 }
